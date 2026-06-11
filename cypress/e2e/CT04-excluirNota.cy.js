ffedb2345ca4cac520f6defcec28d9f52df7c62b
IaraStevani <iarastevanialves@gmail.com>
2026-06-11 10:25:21 -0300

    Ajustando scrip automatizado de exclusão de nota

--- cypress/e2e/CT04-excluirNota.cy.js
+++ cypress/e2e/CT04-excluirNota.cy.js
@@ -1,13 +1,19 @@
-describe('Testes de Alteração', () => {
+describe('Testes de Exclusão', () => {
 
     beforeEach(() => {
         cy.visit('/')
         cy.RealizarLoginComCredenciaisValidas()
     });
 
-    it('Exclusão com sucesso', ()=>{
-        cy.contains('Nota de teste').click()
+    it('Exclusão com sucesso', () => {
+        const noteContent = `Nota para excluir ${Date.now()}`
+
+        cy.get('#new-note-btn').click()
+        cy.get('#note-content').type(noteContent)
+        cy.get('#note-tags').type('work')
+        cy.get('#save-note-btn').click()
+        cy.contains(noteContent).should('be.visible').click()
         cy.get('#delete-note-btn').click()
-        cy.contains('Nota de teste').should('not.exist')
+        cy.contains(noteContent).should('not.exist')
     });
 });
